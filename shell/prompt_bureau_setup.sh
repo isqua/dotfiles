26042a325bb4578a9d4d9bbf0f32b868d5cba3d9
isqua <isqua@isqua.ru>
2015-11-06 00:24:19 +0300

    Сделать правый промпт асинхронным

--- shell/prompt_bureau_setup.sh
+++ shell/prompt_bureau_setup.sh
@@ -21,9 +21,13 @@ prompt_bureau_string_width () {
     echo ${#${(S%%)str//(\%([KF1]|)\{*\}|\%[Bbkf])}}
 }
 
-prompt_bureau_precmd () {
-    prompt_bureau_update_vcs
+prompt_bureau_rprompt_file() {
+    echo "/tmp/$(whoami)_zsh_rprompt.$$"
+}
 
+BUREAU_ASYNC_PROC=0
+
+prompt_bureau_precmd () {
     # user@host path
     local left="%{$fg_bold[$prompt_bureau_user_color]%}%n%{$reset_color%}@%m %{$fg_bold[$prompt_bureau_path_color]%}%~%{$reset_color%}"
     # current time
@@ -31,6 +35,15 @@ prompt_bureau_precmd () {
     local offset=$(( $COLUMNS - $(prompt_bureau_string_width $left) - $(prompt_bureau_string_width $right) + 2 ))
 
     print -P "\n${left}" "${(l:$offset:: :)right}"
+
+    # kill child if necessary
+    if [[ "${BUREAU_ASYNC_PROC}" != 0 ]]; then
+        kill -s HUP $BUREAU_ASYNC_PROC >/dev/null 2>&1 || :
+    fi
+
+    # start background computation
+    prompt_bureau_vcs_prompt &!
+    BUREAU_ASYNC_PROC=$!
 }
 
 prompt_bureau_vcs_style () {
@@ -45,9 +58,23 @@ prompt_bureau_vcs_style () {
     zstyle ':vcs_info:git*' actionformats "[${git_base} %{$fg[magenta]%}⌘ %a%{$reset_color%}]"
 }
 
-prompt_bureau_update_vcs () {
+prompt_bureau_vcs_prompt () {
     vcs_info
-    RPROMPT="$vcs_info_msg_0_"
+    printf "%s" "$vcs_info_msg_0_" > "$(prompt_bureau_rprompt_file)"
+
+    # signal parent
+    kill -s USR1 $$
+}
+
+function TRAPUSR1() {
+    # read from temp file
+    RPROMPT="$(cat $(prompt_bureau_rprompt_file))"
+
+    # reset proc number
+    BUREAU_ASYNC_PROC=0
+
+    # redisplay
+    zle && zle reset-prompt
 }
 
 prompt_bureau_setup $@
