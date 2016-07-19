5ec0dfa8d9e0dbbcf78e772d5ecdcf97f4072849
isqua <isqua@isqua.ru>
2016-07-19 20:02:01 +0300

    Update bash prompt

--- shell/bash_prompt.sh
+++ shell/bash_prompt.sh
@@ -1,43 +1,34 @@
-_prompt_color() {
-    local c=sgr0
+git_prompt_file=/usr/lib/git-core/git-sh-prompt
 
-    case "$1" in
-        "reset")    c="sgr0"       ;;
-        "white")    c="setaf 254"  ;;
-        "red")      c="setaf 124"  ;;
-        "green")    c="setaf 64"   ;;
-        "blue")     c="setaf 33"   ;;
-        "bold")     c="bold"       ;;
-    esac
+if [ -f $git_prompt_file ]; then
 
-    echo "$(tput $c)"
-}
+    source $git_prompt_file
 
-_prompt_git() {
-    ref=$(command git symbolic-ref HEAD 2> /dev/null) || \
-    ref=$(command git rev-parse --short HEAD 2> /dev/null) || return
-    echo "$(_prompt_color blue)${ref#refs/heads/}$(_prompt_color reset)"
-}
+    GIT_PS1_SHOWDIRTYSTATE=y
+    GIT_PS1_SHOWUNTRACKEDFILES=y
+    GIT_PS1_SHOWCOLORHINTS=y
 
-# returns user@host for prompt
-_prompt_user() {
-    echo "$(_prompt_color bold)$(_prompt_color white)\u$(_prompt_color reset)@\H"
-}
+    PROMPT_COMMAND='__git_ps1 "\n\e[1;37m\u\e[0m@\H \e[1;37m\w\e[0m " "\n> \$ "'
 
-# returns current path for prompt (home is ~)
-_prompt_path() {
-    echo "$(_prompt_color bold)$(_prompt_color white)\w$(_prompt_color reset)"
-}
+else
 
-# returns green $ for users and red # for root
-_prompt_prompt() {
-    local color=green
-    if [ "$UID" = "0" ]; then color=red; fi
-    echo "$(_prompt_color ${color})\$$(_prompt_color reset) "
-}
+    Color_Off='\e[0m'       # Text Reset
 
-PS1="\n"
-PS1+="$(_prompt_user) "
-PS1+="$(_prompt_path) "
-PS1+="$(_prompt_git) "
-PS1+="\n$(_prompt_prompt)"
+    # Bold
+    BWhite='\e[1;37m'       # White
+
+    _prompt_user() {
+        echo "${BWhite}\u${Color_Off}@\H"
+    }
+
+    # returns current path for prompt (home is ~)
+    _prompt_path() {
+        echo "${BWhite}\w${Color_Off}"
+    }
+
+    PS1="\n"
+    PS1+="$(_prompt_user) "
+    PS1+="$(_prompt_path)"
+    PS1+="\n> \$ "
+
+fi
