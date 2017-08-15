d0273f469f17e09f7ac8b0d6a8696e7317a82b6d
isqua <isqua@isqua.ru>
2017-08-15 19:00:16 +0300

    Fix broken symlink ls color

--- shell/export.sh
+++ shell/export.sh
@@ -1,6 +1,6 @@
 export LESSCHARSET=utf-8
 
-export LS_COLORS=$LS_COLORS:'di=34:fi=0:ln=4:or=33:mi=red:ex=35'
+export LS_COLORS=$LS_COLORS:'di=34:fi=0:ln=4:or=33:ex=35'
 # on Mac OS X you should install coreutils
 export LS_OPTIONS='--color'
 
