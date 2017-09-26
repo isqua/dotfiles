601cb6536b7870b13a35a0ee5a0be7cbe26e5ea0
isqua <isqua@isqua.ru>
2017-09-27 01:48:52 +0300

    Supports nvm on mac os

--- shell/export.sh
+++ shell/export.sh
@@ -8,5 +8,10 @@ export LS_OPTIONS='--color'
 export GOPATH="$HOME/go"
 export PATH="$PATH:$GOPATH/bin"
 
-export NVM_DIR="/home/isqua/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+# Node Version Manager https://github.com/creationix/nvm
+export NVM_DIR="$HOME/.nvm"
+if [ -s "$NVM_DIR/nvm.sh" ]; then
+    source "$NVM_DIR/nvm.sh"
+elif [ -s "/usr/local/opt/nvm/nvm.sh" ]; then
+    source "/usr/local/opt/nvm/nvm.sh"
+fi
