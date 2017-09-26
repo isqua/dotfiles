b33261fe1711ff34353283dd5e51a0bae44a3faf
isqua <isqua@users.noreply.github.com>
2017-09-27 01:30:16 +0300

    Add alias to open files

---
+++ shell/detect_os.sh
@@ -0,0 +1,8 @@
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
