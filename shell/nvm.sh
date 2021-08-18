c48bb3d6e697581e3e310d9e98b8055149731049
isqua <isqua@isqua.ru>
2021-08-18 19:56:43 +0300

    unify brew path

--- shell/nvm.sh
+++ shell/nvm.sh
@@ -1,13 +1,15 @@
 export NVM_DIR="$HOME/.nvm"
 
 # Macos + Homebrew
-[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && . "/opt/homebrew/opt/nvm/nvm.sh" # This loads nvm
-[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && . "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
+mac_nvm_root=$(brew --prefix nvm)
+[ -s "${mac_nvm_root}/nvm.sh" ] && . "${mac_nvm_root}/nvm.sh" # This loads nvm
+[ -s "${mac_nvm_root}/etc/bash_completion.d/nvm" ] && . "${mac_nvm_root}/etc/bash_completion.d/nvm" # This loads nvm bash_completion
 
 # Linux
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+# For Apple M1
 function nvm_install() {
     version=$1
     uname=$(uname -a)
@@ -19,7 +21,7 @@ function nvm_install() {
 
     if [[ $uname == *"Darwin"* && $uname == *"ARM64"* ]]; then
         echo "Install node for x86_64"
-        arch -x86_64 bash -c "source /opt/homebrew/opt/nvm/nvm.sh && nvm install ${version}"
+        arch -x86_64 bash -c "source ${mac_nvm_root}/nvm.sh && nvm install ${version}"
     else
         nvm install ${version}
     fi
