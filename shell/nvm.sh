865c72470dc9839a1c3a535c9577964646b1bbc5
isqua <isqua@isqua.ru>
2021-08-18 17:25:15 +0300

    install nvm for x86_64 on m1

--- shell/nvm.sh
+++ shell/nvm.sh
@@ -7,3 +7,20 @@ export NVM_DIR="$HOME/.nvm"
 # Linux
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+function nvm_install() {
+    version=$1
+    uname=$(uname -a)
+
+    if [[ -z "${version}" ]]; then
+        nvm_rc_version
+        version=$NVM_RC_VERSION
+    fi
+
+    if [[ $uname == *"Darwin"* && $uname == *"ARM64"* ]]; then
+        echo "Install node for x86_64"
+        arch -x86_64 bash -c "source /opt/homebrew/opt/nvm/nvm.sh && nvm install ${version}"
+    else
+        nvm install ${version}
+    fi
+}
